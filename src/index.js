913231d1f2485401c1465eb8fe940138802e5934
अक्षय बढे <aqua26@gmail.com>
2020-08-05 12:05:50 +0530

    Master

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ class App extends React.Component{
     constructor(props) {
         super(props);
         
+        // This is the only time we do directly assignment
+        // to this.state
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we Called Setstate !!!!!
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => console.log(err)
+        );
     }
 
     // React says we have to define render !!
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
-    
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat} </div>;
     }
 }
 
